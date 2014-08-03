938c520a358f6a452f643d8dd1f943f45f89c5f4
Grégory Mounié <Gregory.Mounie@imag.fr>
2014-08-03 18:00:17 +0200

    utilisation du lp

---
+++ src/tsp-lp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/* sauvergarde dans un fichier d'un programme linéaire (Format LP)
+   calculant une borne inférieur sur la longueur du TSP de la solution
+   partielle */
+void save_lp(FILE* f, tsp_path_t path, int len) {
+  return;
+};
+
+/* Calcul d'une borne inférieure en sauvegardant dans un fichier et en
+   lancant un solveur linéaire (cbc) sur le fichier et en interprétant
+   son résultat */ 
+
+int lower_bound_using_lp(tsp_path_t path, int len) {
+
+}
